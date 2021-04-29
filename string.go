987bac77b339863f6d2ee2d8c0300af2502a9f1e
Milad Ebrahimi <miladibra10@gmail.com>
2021-04-29 23:00:33 +0430

    feat: implement string validation

---
+++ string.go
@@ -0,0 +1,126 @@
+package vjson
+
+import (
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
+	"regexp"
+	"strings"
+)
+
+type StringField struct {
+	name     string
+	required bool
+
+	validateMinLength bool
+	minLength         int
+
+	validateMaxLength bool
+	maxLength         int
+
+	validateFormat bool
+	format         string
+
+	validateChoices bool
+	choices         []string
+}
+
+// To Force Implementing Field interface by StringField
+var _ Field = (*StringField)(nil)
+
+func (s *StringField) Required() *StringField {
+	s.required = true
+	return s
+}
+
+func (s *StringField) MinLength(length int) *StringField {
+	if length < 0 {
+		return s
+	}
+	s.minLength = length
+	s.validateMinLength = true
+	return s
+}
+
+func (s *StringField) MaxLength(length int) *StringField {
+	if length < 0 {
+		return s
+	}
+	s.maxLength = length
+	s.validateMaxLength = true
+	return s
+}
+
+func (s *StringField) Format(format string) *StringField {
+	s.format = format
+	s.validateFormat = true
+	return s
+}
+
+func (s *StringField) Choices(choices ...string) *StringField {
+	s.choices = choices
+	s.validateChoices = true
+	return s
+}
+
+func (s *StringField) Validate(value interface{}) error {
+	if value == nil {
+		if !s.required {
+			return nil
+		} else {
+			return errors.Errorf("Value for %s field is required", s.name)
+		}
+	}
+
+	stringValue, ok := value.(string)
+
+	if !ok {
+		return errors.Errorf("Value for %s should be a string", s.name)
+	}
+
+	var result error
+
+	if s.validateMinLength {
+		if len(stringValue) < s.minLength {
+			result = multierror.Append(result, errors.Errorf("Value for %s field should have at least %d characters", s.name, s.minLength))
+		}
+	}
+
+	if s.validateMaxLength {
+		if len(stringValue) > s.maxLength {
+			result = multierror.Append(result, errors.Errorf("Value for %s field should have at most %d characters", s.name, s.maxLength))
+		}
+	}
+
+	if s.validateChoices {
+		for _, choice := range s.choices {
+			if stringValue == choice {
+				return nil
+			}
+		}
+		result = multierror.Append(result, errors.Errorf("Value for %s field should be one of: [%s] values", s.name, strings.Join(s.choices, ",")))
+	}
+
+	if s.validateFormat {
+		r, err := regexp.Compile(s.format)
+		if err != nil {
+			result = multierror.Append(result, errors.Wrapf(err, "Invalid StringField format string for field %s", s.name))
+			return result
+		}
+
+		isValidFormat := r.MatchString(stringValue)
+
+		if !isValidFormat {
+			result = multierror.Append(result, errors.Wrapf(err, "Invalid StringField format string for field %s", s.name))
+		}
+	}
+
+	return result
+}
+
+func String(name string) *StringField {
+	return &StringField{
+		name:     name,
+		required: false,
+		choices:  []string{},
+	}
+}
---
+++ string_test.go
@@ -0,0 +1,124 @@
+package vjson
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringField_Validate(t *testing.T) {
+	t.Run("invalid_input", func(t *testing.T) {
+		field := String("foo")
+
+		err := field.Validate(1)
+		assert.NotNil(t, err)
+	})
+	t.Run("not_required_field", func(t *testing.T) {
+		t.Run("nil_value", func(t *testing.T) {
+			field := String("foo")
+
+			err := field.Validate(nil)
+			assert.Nil(t, err)
+		})
+		t.Run("valid_value", func(t *testing.T) {
+			field := String("foo")
+
+			err := field.Validate("Hi")
+			assert.Nil(t, err)
+		})
+	})
+	t.Run("required_field", func(t *testing.T) {
+		t.Run("nil_value", func(t *testing.T) {
+			field := String("foo").Required()
+
+			err := field.Validate(nil)
+			assert.NotNil(t, err)
+		})
+		t.Run("valid_value", func(t *testing.T) {
+			field := String("foo").Required()
+
+			err := field.Validate("Hi")
+			assert.Nil(t, err)
+		})
+	})
+	t.Run("min_length", func(t *testing.T) {
+		t.Run("invalid_length", func(t *testing.T) {
+			field := String("foo").MinLength(-1)
+
+			assert.Equal(t, false, field.validateMinLength)
+			assert.Equal(t, 0, field.minLength)
+		})
+		t.Run("valid_length", func(t *testing.T) {
+			field := String("foo").MinLength(5)
+
+			t.Run("valid_input", func(t *testing.T) {
+				err := field.Validate("12345")
+				assert.Nil(t, err)
+			})
+			t.Run("invalid_input", func(t *testing.T) {
+				err := field.Validate("1234")
+				assert.NotNil(t, err)
+			})
+		})
+	})
+	t.Run("max_length", func(t *testing.T) {
+		t.Run("invalid_length", func(t *testing.T) {
+			field := String("foo").MaxLength(-1)
+
+			assert.Equal(t, false, field.validateMaxLength)
+			assert.Equal(t, 0, field.maxLength)
+		})
+		t.Run("valid_length", func(t *testing.T) {
+			field := String("foo").MaxLength(5)
+
+			t.Run("valid_input", func(t *testing.T) {
+				err := field.Validate("123")
+				assert.Nil(t, err)
+			})
+			t.Run("invalid_input", func(t *testing.T) {
+				err := field.Validate("123456")
+				assert.NotNil(t, err)
+			})
+		})
+	})
+	t.Run("choices", func(t *testing.T) {
+		t.Run("valid_choice", func(t *testing.T) {
+			field := String("foo").Choices("A", "B")
+
+			err := field.Validate("A")
+			assert.Nil(t, err)
+
+			err = field.Validate("B")
+			assert.Nil(t, err)
+
+			err = field.Validate("AB")
+			assert.NotNil(t, err)
+		})
+	})
+	t.Run("format", func(t *testing.T) {
+		t.Run("valid_format", func(t *testing.T) {
+
+			field := String("foo").Format("p([a-z]+)ch")
+
+			err := field.Validate("peach")
+			assert.Nil(t, err)
+
+			err = field.Validate("pach")
+			assert.Nil(t, err)
+
+			err = field.Validate("HI")
+			assert.NotNil(t, err)
+
+			err = field.Validate("foo")
+			assert.NotNil(t, err)
+		})
+		t.Run("invalid_format", func(t *testing.T) {
+
+			field := String("foo").Format(")(")
+
+			err := field.Validate("peach")
+			assert.NotNil(t, err)
+
+		})
+
+	})
+}
